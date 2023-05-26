eaeec49cf32e67069b86122f897d857c6bae2a5b
brian <briancorrea@gmail.com>
2023-05-26 13:09:49 -0400

    wip

---
+++ library/create-slideshow.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if the script is called with the correct number of arguments
+if [ "$#" -eq 0 ]; then
+    echo "Error: No filenames provided."
+    echo "Usage: ./script.sh <filename1>"
+    exit 1
+fi
+
+if [ "$#" -lt 1 ]; then
+    echo "Error: Insufficient number of arguments for filenames. Need at least one file."
+    echo "Usage: ./script.sh <filename1>"
+    exit 1
+fi
+
+# Store the filenames
+arguments=("$@")
+
+# Extract filenames
+# video_file="${arguments[0]}"
+
+# ffmpeg -report -i "$video_file" -vf fps=$fps "${video_file%.*}"_%d.png
