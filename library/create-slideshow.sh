d7ba2125f69c63bb5def22cb05a0b30c33745d7f
brian <briancorrea@gmail.com>
2023-06-02 17:02:10 -0400

    working + ignore log files

--- library/create-slideshow.sh
+++ library/create-slideshow.sh
@@ -7,8 +7,9 @@ if [ "$#" -eq 0 ]; then
     exit 1
 fi
 
-if [ "$#" -lt 2 ]; then
-    echo "Error: Insufficient number of arguments for filenames. Need at least one file."
+# TODO add error message
+if [ "$#" -lt 4 ]; then
+    echo "Error: Insufficient number of arguments for filenames. Need at least TODO: fill this out."
     echo "Usage: ./script.sh <filename1>"
     exit 1
 fi
@@ -21,21 +22,22 @@ transition="${arguments[0]}"
 duration="${arguments[1]}"
 fileArgs="${@:3}"
 
-echo "${transition}"
-echo "${fileArgs}"
+# TODO
+# Check valid transition
+# Check duration limit
+# Check min fileArg length
 
 IFS=' ' read -ra file_paths <<< "$fileArgs"
 
-# ffmpeg -report -i "$video_file" -vf fps=$fps "${video_file%.*}"_%d.png
-command="ffmpeg -hide_banner"
+command="ffmpeg -report -y -hide_banner"
 for f in "${file_paths[@]}"
 do
     command="$command -loop 1 -i $f -t $duration"
 done
 
-filter_complex="[0:v][1:v]xfade=transition=hrslice:duration=1:offset=3 [p0]"
+filter_complex="[0:v][1:v]xfade=transition=${transition}:duration=1:offset=3[p0]"
 
-final_command="$command '$filter_complex' -r 30 -c:v libx264 -map [p0] -pix_fmt yuv420 -t $duration assets/out.mp4"
+final_command="$command -filter_complex $filter_complex -r 30 -c:v libx264 -map [p0] -pix_fmt yuv420p -t $duration -preset superfast assets/slideshow.mp4"
 
 $final_command
 
